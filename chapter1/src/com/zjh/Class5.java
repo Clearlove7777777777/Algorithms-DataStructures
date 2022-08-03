77b94e9199c71162d4020ed4b1946110efa24f02
Clearlove7777777777 <1227819307@qq.com>
2022-08-03 17:11:25 +0800

    Node

--- chapter1/src/com/zjh/Class5.java
+++ chapter1/src/com/zjh/Class5.java
@@ -1,7 +1,14 @@
 package com.zjh;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
 import java.util.Stack;
 
 /**
@@ -114,6 +121,76 @@ public class Class5 {
         }
     }
 
+    /**
+     * 二叉树的宽度优先遍历（层序遍历）
+     * 使用队列（先进先出）,入队一个节点，
+     * @param node
+     */
+    public static void wideOrder(Node node){
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+        while (!queue.isEmpty()){
+            Node poll = queue.poll();
+            System.out.println(poll.value);
+            if (poll.left != null) {
+                queue.add(poll.left);
+            }
+            if (poll.right != null) {
+                queue.add(poll.right);
+            }
+        }
+    }
+
+    /**
+     * 找到二叉树的最大宽度层以及该层的数量
+     * 使用一个map，可以记录每一个节点是第几层
+     * @param node
+     */
+    public static void findMaxWidthUseMap(Node node){
+        Map<Node,Integer> map = new HashMap<>(); 
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(node);
+        
+        int maxLevel = 1;
+        int maxLevelCount = 1;
+        int curLevel = 1;
+        int curLevelCount = 0;
+        map.put(node,curLevel);
+        while (!queue.isEmpty()){
+            Node poll = queue.poll();
+            if (map.get(poll) == curLevel){
+                // 在同一层
+                curLevelCount++;
+            }else {
+                // 新的层
+                maxLevel = curLevelCount > maxLevelCount ? curLevel : maxLevel;
+                maxLevelCount = Math.max(curLevelCount,maxLevelCount);
+                curLevel++;
+                curLevelCount = 1;
+            }
+            if (poll.left != null) {
+                map.put(poll.left,curLevel+1);
+                queue.add(poll.left);
+            }
+            if (poll.right != null) {
+                map.put(poll.right,curLevel+1);
+                queue.add(poll.right);
+            }
+        }
+        // 最后一层的数量在循环内没有被比较到，最后再判断一次
+        boolean flag = maxLevelCount < curLevelCount;
+        System.out.println("maxLevel:"+(flag?++maxLevel:maxLevel)+",maxLevelCount:"+(flag?curLevelCount:maxLevelCount));
+    }
+
+    /**
+     * 找到二叉树的最大宽度层以及该层的数量
+     * 不使用map
+     * @param node
+     */
+    public static void findMaxWidth(Node node){
+        
+    }
+
     public static void main(String[] args) {
         Node n1 = new Node(1);
         Node n2 = new Node(2);
@@ -134,6 +211,32 @@ public class Class5 {
         posOrderUnRecur(n1);
         System.out.println("===preOrderUnRecur===");
         preOrderUnRecur(n1);
+        System.out.println("===wideOrder===");
+        wideOrder(n1);
+        findMaxWidthUseMap(n1);
+        System.out.println("==========================");
+        
+        Set<String> set = new HashSet<>();
+        set.add("1");
+        set.add("12");
+        Object[] array = set.toArray();
+        String[] array1 = set.toArray(new String[0]);
+        for (Object o : array) {
+            System.out.println(o);
+        }
+        for (String s : array1) {
+            System.out.println(s);
+        }
+        System.out.println("=================");
+        int[] a = new int[]{1,2,0,3};
+        for (int i : a) {
+            try {
+                System.out.println(1/i);
+                System.out.println(i);
+            }catch (Exception e){
+                
+            }
+        }
     }
 }
 
